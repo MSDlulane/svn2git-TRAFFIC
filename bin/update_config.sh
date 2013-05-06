149044ade7ce831e0f5650fe7211c5ddffa61f09
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-06 07:37:46 +0000

    Changing version control scripting: update_config.sh
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@5 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

--- bin/update_config.sh
+++ bin/update_config.sh
@@ -3,11 +3,11 @@ TRAFHOME=/opt/mhg/TRAFFIC
 INITIAL_REV=$(svn info "$TRAFHOME" | grep Revision |awk '{print $2}')
 EXP_NEXT_REV=$((INITIAL_REV+1))
 cd $TRAFHOME
-svn update
+svn update "$TRAFHOME"
 NEW_REV=$(svn info "$TRAFHOME" | grep Revision |awk '{print $2}')
 if [ "$INITIAL_REV" = "$NEW_REV" ]; then
 	echo "No changes were detected."
 else
 	echo "Summary of (A)dded and (M)odified files."
-	svn diff "$EXP_NEXT_REV":"$NEW_REV" |egrep "^   (A|M)"
+	svn log -v -r "$EXP_NEXT_REV":"$NEW_REV" "$TRAFHOME" |egrep "^   (A|M)"
 fi
