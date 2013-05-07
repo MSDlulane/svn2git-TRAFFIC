926387d8edf678bc37a3fe0a4e926ae8af4e3ca8
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-07 05:07:45 +0000

    Commented bin/deploy.sh
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@27 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
-/opt/mhg/TRAFFIC/bin/svc.sh Mhg_ACPT restart
+###
+# The SVN revision number should be supplied for
+# configuration rollback
+##
+REVISION=$1
+###
+# Only the shell utilities listed below 
+# should be used in this script.
+#
+# - /opt/mhg/TRAFFIC/bin/all_install.sh
+# - /opt/mhg/TRAFFIC/bin/all_uninstall.sh
+# - /opt/mhg/TRAFFIC/bin/debug_startup.sh
+# - /opt/mhg/TRAFFIC/bin/deploy.sh
+# - /opt/mhg/TRAFFIC/bin/install.sh
+# - /opt/mhg/TRAFFIC/bin/list_traf.sh
+# - /opt/mhg/TRAFFIC/bin/perform_deploy.sh
+# - /opt/mhg/TRAFFIC/bin/remote.sh
+# - /opt/mhg/TRAFFIC/bin/svc.sh
+# - /opt/mhg/TRAFFIC/bin/svctraf.sh
+# - /opt/mhg/TRAFFIC/bin/traf.sh
+# - /opt/mhg/TRAFFIC/bin/traf-debug.sh
+# - /opt/mhg/TRAFFIC/bin/uninstall.sh
+# - /opt/mhg/TRAFFIC/bin/update_config.sh
+# - /opt/mhg/TRAFFIC/bin/verify_configs.sh
+####
+
+###
+# Uncomment the line below to update the config:
+#/opt/mhg/TRAFFIC/bin/update_config.sh ${REVISION}
+
+###
+# The line below will restart the "Mhg_ACPT" TRAF 
+# server. To modify this, find the traffic server
+# name in the file "$TRAFHOME/etc/traffic_server_list.txt"
+# and replace the name
+#/opt/mhg/TRAFFIC/bin/svc.sh Mhg_ACPT restart
+echo "Done."
