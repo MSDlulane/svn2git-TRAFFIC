39a493ab49c2c5a06a935ab2512d6455cf2ff5d0
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-07 04:56:24 +0000

    improving remote logging.
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@25 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

--- bin/remote.sh
+++ bin/remote.sh
@@ -10,6 +10,7 @@ if [ "$REV_NR" = "" ]; then
 else
 	echo "Performing rollback..."
 fi
+LOGFILE="${OPERATION}${DATESTAMP}"
 
 if [ "$SSH_USER" = "" ]; then
 	echo "No SSH user specified. Abort."
@@ -19,11 +20,12 @@ else
 	IFS=$'\r\n'
 	for hostname in $(grep -v "^#" "$TRAFHOME/etc/traffic_server_list.txt" | awk '{print $1}' | sort | uniq)
 	do
-		ssh -t "${SSH_USER}@${hostname}" "sudo $TRAFHOME/bin/perform_deploy.sh $REV_NR" |tee "${OPERATION}${DATESTAMP}"
+		ssh -t "${SSH_USER}@${hostname}" "sudo $TRAFHOME/bin/perform_deploy.sh $REV_NR" 2>&1 | tee "${LOGFILE}"
 	done
 	IFS=$OLD_IFS
 fi
-echo "Adding deployment log to SVN repository..."
-svn add ./* 
-svn ci -m "Deployment completed: ${SSH_USER}."
-echo "Done."
+echo "Adding deployment log to SVN repository..." |tee -a "${LOGFILE}"
+svn add ./* 2>&1 | grep -v "is already under version control" |tee -a "${LOGFILE}"
+svn ci -m "Deployment completed: ${SSH_USER}." |tee -a "${LOGFILE}"
+echo "Done."|tee -a "${LOGFILE}"
+
