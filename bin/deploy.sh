0e93e1dd278f81f75ffb015e121af405c1800053
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-24 14:37:03 +0000

    Deployment completed: jmeyer.
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@86 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,14 +13,14 @@ fi
 
 while true; do
 	if [ $1 ]; then
-		echo 'ssh -t "${trafUser}@$1" "sudo $trafHome/bin/perform_deploy.sh"' '2>&1' '| tee "${logFile}"'
+		echo ssh -t "${trafUser}@$1" "sudo $trafHome/bin/perform_deploy.sh" '2>&1' '| tee "${logFile}"'
 		shift
 	else
 		break
 	fi	
 done
 echo "Adding deployment log to SVN repository..." |tee -a "${logFile}"
-svn add ./* 2>&1 | grep -v "is already under version control" |tee -a "${logFile}"
+#svn add ./* 2>&1 | grep -v "is already under version control" |tee -a "${logFile}"
 svn ci -m "Deployment completed: ${sshUser}." |tee -a "${logFile}"
 svn update
 echo "Done."
