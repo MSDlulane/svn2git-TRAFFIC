1cd59ef4daac9e50fbf36f356548f32526378419
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-27 14:51:28 +0000

    Adding test and debug scripts
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@116 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

---
+++ dbin/debug-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+testhostname=$1
+trafficdirname=$2
+
+for trafname in $(grep "^${testhostname}" ${trafficdirname}/conf/traffic_server_list.txt | awk '{print $3}')
+do
+	echo "Starting \"${trafname}\" for test..."
+	${trafficdirname}/dbin/traf-debuf.sh ${testhostname} ${trafficdirname} ${trafname}
+done
---
+++ dbin/traf-debug.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+HOSTNAME=$1
+TRAFHOME=$2
+TRAFNAME=$3
+TRAFPROCNAME=traffic-$TRAFNAME
+
+TRAFLIST=$TRAFHOME/conf/traffic_server_list.txt
+TRAFLIB=$(grep "$HOSTNAME $TRAFNAME " $TRAFLIST | awk '{print $3}')
+TRAFENABLED=$(grep "$HOSTNAME $TRAFNAME " $TRAFLIST | awk '{print $4}')
+TRAFSTARTMEM=$(grep "$HOSTNAME $TRAFNAME " $TRAFLIST | awk '{print $5}')
+TRAFMAXMEM=$(grep "$HOSTNAME $TRAFNAME " $TRAFLIST | awk '{print $6}')
+TRAFJMXPORT=$(grep "$HOSTNAME $TRAFNAME " $TRAFLIST | awk '{print $7}')
+TRAFDIR=$TRAFHOME/etc/$TRAFNAME
+
+echo ""
+echo "Traffic Server Test only startup."
+if [ "$TRAFENABLED" = "ENABLED" ]; then
+	echo "$TRAFPROCNAME Service is enabled."
+else
+	if [ "$TRAFENABLED" = "" ]; then
+		echo "$TRAFPROCNAME Service is NOT listed in \"$TRAFLIST\". Abort."
+		exit 1
+	fi
+	echo "$TRAFPROCNAME Service is NOT enabled. TRAF will not start. Abort."
+	exit 2
+fi
+
+TRAFJMXSETTINGS=""
+TRAFSNMPSETTINGS=""
+if [ "$TRAFJMXPORT" = "" ]; then
+	echo "TRAF JMX/SNMP port is not configured. TRAF will not start. Abort."
+	exit 4
+else
+	echo "TRAF JMX [_ONLY_] port set to #$TRAFJMXPORT."
+	TRAFJMXSETTINGS="-Dcom.sun.management.jmxremote.port=$TRAFJMXPORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+fi
+
+if [ -d  "$TRAFHOME/lib/$TRAFLIB" ]; then
+	echo "Using library \"$TRAFLIB\"."
+else
+	echo "\"$TRAFLIB\" does not exist. Abort."
+	exit 8 
+fi
+
+TRAFSTARTMEM_OPT=""
+echo "Ignoring JVM start heap-size."
+
+TRAFMAXMEM_OPT=""
+echo "Ignoring JVM max heap-size."
+
+TRAFMEMSETTINGS="$TRAFSTARTMEM_OPT $TRAFMAXMEM_OPT"
+
+cd "$TRAFDIR"
+nohup java -server $TRAFMEMSETTINGS $TRAFSNMPSETTINGS $TRAFJMXSETTINGS -jar "$TRAFHOME/lib/$TRAFLIB/TRAF.jar" "$TRAFDIR/config.xml" >"$TRAFDIR/log/stdout" 2>"$TRAFDIR/log/stderr" &
+echo $! > /var/run/mhgsvc/${TRAFPROCNAME}.pid
