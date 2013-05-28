83f7caefac88d3fb1ea26e0cc01773dd3e071414
(no author) <(no author)@4db9f1c2-9d7c-4cd1-a05b-62adee338cff>
2013-05-28 13:06:14 +0000

    append to deployment and rollback logs
    git-svn-id: https://cptlsvn01.za.mhgad.com/repos/TRAFFIC@139 4db9f1c2-9d7c-4cd1-a05b-62adee338cff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,7 +13,7 @@ fi
 
 while true; do
 	if [ $1 ]; then
-		ssh -t "${sshUser}@$1" "sudo $trafHome/bin/perform_deploy.sh" 2>&1 | tee "${logFile}"
+		ssh -t "${sshUser}@$1" "sudo $trafHome/bin/perform_deploy.sh" 2>&1 | tee -a "${logFile}"
 		shift
 	else
 		break
--- bin/rollback.sh
+++ bin/rollback.sh
@@ -20,7 +20,7 @@ fi
 
 while true; do
 	if [ $1 ]; then
-		ssh -t "${sshUser}@$1" "sudo $trafHome/bin/perform_deploy.sh ${rollbackRev}" 2>&1 | tee "${logFile}"
+		ssh -t "${sshUser}@$1" "sudo $trafHome/bin/perform_deploy.sh ${rollbackRev}" 2>&1 | tee -a "${logFile}"
 		shift
 	else
 		break
